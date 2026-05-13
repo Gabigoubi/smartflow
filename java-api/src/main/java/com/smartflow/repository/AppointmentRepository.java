f0c08e84bbcba74710c280b8203349c019cab458
Gabriel de Abreu <gabrielabreu2690@gmail.com>
2026-05-13 19:08:57 -0300

    Inclusão da lógica de agendamento

--- java-api/src/main/java/com/smartflow/repository/AppointmentRepository.java
+++ java-api/src/main/java/com/smartflow/repository/AppointmentRepository.java
@@ -1,1 +1,9 @@
-.
+package com.smartflow.repository;
+
+import com.smartflow.entity.Appointment; 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
