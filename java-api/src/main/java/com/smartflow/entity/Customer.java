847fc551cf59e7bb8c209485ee3fbdc486f8d308
Gabriel de Abreu <gabrielabreu2690@gmail.com>
2026-05-13 07:29:55 -0300

    Inclusão inicial da classe Customer

--- java-api/src/main/java/com/smartflow/entity/Customer.java
+++ java-api/src/main/java/com/smartflow/entity/Customer.java
@@ -1,1 +1,18 @@
-.
+package com.smartflow.entity;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+@Id 
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+@Column(name = "car_model", nullable = false)
+private String carModel;
+@Column(name = "name", nullable = false)
+private String name;
+@Column(name = "next_revision_date", nullable = false)
+private LocalDate nextRevisionDate;
+}
+
