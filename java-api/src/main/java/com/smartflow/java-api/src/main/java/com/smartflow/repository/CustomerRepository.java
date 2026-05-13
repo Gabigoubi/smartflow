f60d11383fa88760a8340f59ba68d7cb7bb5784b
Gabriel de Abreu <gabrielabreu2690@gmail.com>
2026-05-13 18:55:01 -0300

    Delete java-api/src/main/java/com/smartflow/java-api directory
    Erro

--- java-api/src/main/java/com/smartflow/java-api/src/main/java/com/smartflow/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartflow.repository;
-
-import com.smartflow.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
