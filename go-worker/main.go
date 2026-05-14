afc479b867bb325d89876d89b537c56fe440be01
Gabriel de Abreu <gabrielabreu2690@gmail.com>
2026-05-14 11:18:52 -0300

    Incluída lógica de validação e log visual

--- go-worker/main.go
+++ go-worker/main.go
@@ -1,28 +1,66 @@
-package main
+func runWorker(db *sql.DB) {
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
 
-import (
-	"database/sql"
-	"fmt"
-	"log"
+	for range ticker.C {
+		fmt.Println("Buscando clientes....")
+		query := `
+		SELECT c.id, c.name, a.status, a.schedule_date
+		FROM customers c
+		LEFT JOIN appointments a ON c.id = a.customer_id
+		`
+		rows, err := db.Query(query)
+		if err != nil {
+			fmt.Println("Erro ao buscar no banco: ", err)
+			continue
+		}
 
-	_ "github.com/lib/pq"
-)
+		for rows.Next() {
+			var id int
+			var name string
+			var status sql.NullString
+			var scheduleDate sql.NullTime
 
-func main() {
-	
-	connStr := "user=root password=root dbname=concessionaria_db host=5432 port=5432 sslmode=disable"
+			err := rows.Scan(&id, &name, &status, &scheduleDate)
+			if err != nil {
+				fmt.Println("Erro ao extrair dados da linha: ", err)
+				continue
+			}
 
-	
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal("Erro fatal ao configurar o banco: ", err)
+			days := int(time.Since(scheduleDate.Time).Hours() / 24)
+
+			if status.Valid && status.String == "ABERTO" {
+				fmt.Printf("Cliente [%s] já tem revisão agendada -> IGNORADO\n", name)
+				continue
+			}
+
+			if scheduleDate.Valid && days <= 90 {
+				fmt.Printf("Cliente [%s] já tem agendamento em menos de 90 dias -> IGNORADO\n", name)
+				continue
+			}
+
+			var leadExist bool
+			err = db.QueryRow("SELECT EXISTS(SELECT 1 FROM leads WHERE customer_id = $1)", id).Scan(&leadExist)
+			if err != nil {
+				fmt.Println("Erro ao checar lead duplicado: ", err)
+				continue
+			}
+
+			if leadExist {
+				fmt.Printf("Cliente [%s] já possui um lead ativo -> IGNORADO\n", name)
+				continue
+			}
+
+			fmt.Printf("Cliente [%s] elegível -> LEAD CRIADO COM SUCESSO\n", name)
+
+			time.Sleep(100 * time.Millisecond)
+
+			_, err = db.Exec("INSERT INTO leads (customer_id, created_at) VALUES ($1, NOW())", id)
+			if err != nil {
+				fmt.Println("Erro ao salvar lead no banco: ", err)
+			}
+		}
+		
+		rows.Close()
 	}
-	defer db.Close() 
-	
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("O banco não respondeu ao chamado: ", err)
-	}
-
-	fmt.Println("Conexão com o PostgreSQL estabelecida com sucesso! O Motor Go está online.")
 }
